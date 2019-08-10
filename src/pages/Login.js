129c8991da4d824d3433ccf0f28341cc94be2442
Dino Tudor <dinotudor@gmail.com>
2019-08-10 22:37:46 +0200

    login form and button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
 import logo from './../assets/logo.png'
 
@@ -7,6 +7,14 @@ export default function Login() {
   return(
     <View style={styles.container}>
       <Image source={logo}/>
+      <TextInput
+        placeholder="Enter your Github user"
+        placeholderTextColor="#999"
+        style={styles.input}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,6 +24,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f5f5f5',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 30
+  },
+  input: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#ddd',
+    marginTop: 20,
+    paddingHorizontal: 15,
+  },
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor :'#DF4723',
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#FFF',
   },
 });
