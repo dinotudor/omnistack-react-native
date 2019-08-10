e09e1b75b98637684f0856bc01687704205580cc
Dino Tudor <dinotudor@gmail.com>
2019-08-10 20:35:27 +0200

    add styles

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <Text>Hello World!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+  flex: 1,
+  flexDirection: 'row',
+  backgroundColor: '#7159c1',
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+
+  text: {
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 20
+  },
+})
