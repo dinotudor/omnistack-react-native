bb73966ca0380e7e5ca258df6bd48fefda9b21c9
Dino Tudor <dinotudor@gmail.com>
2019-08-11 00:24:49 +0200

    asyncstorage dep

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView, Image } from 'react-native';
+
+import logo from './../assets/logo.png';
 
 export default function Main() {
-  return <View />
+  return (
+    <SafeAreaView>
+      <Image source={logo}/>
+    </SafeAreaView>
+    );
 }
