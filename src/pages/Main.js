677d0a272f85554a3bac49d45f2c0ea5165cf4c5
Dino Tudor <dinotudor@gmail.com>
2019-08-11 13:03:37 +0200

    fetch users, map, conditional for empty users array

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,29 +7,66 @@ import logo from './../assets/logo.png';
 import dislike from './../assets/dislike.png';
 import like from './../assets/like.png';
 
-export default function Main() {
+export default function Main({ navigation }) {
+  const id = navigation.getParam('user');
+  const [users, setUsers] = useState([]);
+
+  console.log(id)
+
+  useEffect(() => {
+    async function loadUsers(){
+      const response = await api.get('/devs', {
+        headers: {
+          user: id,
+        }
+       })
+
+       setUsers(response.data)
+    }
+    loadUsers();
+  }, [id]);
+
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: { user: id }
+    })
+    setUsers(users.filter(user => user._id !== id ));
+  }
+
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: { user: id }
+    })
+    setUsers(users.filter(user => user._id !== id ));
+  }
+
+
   return (
     <SafeAreaView style={styles.container}>
       <Image style={styles.logo} source={logo}/>
-
-      <View style={[styles.cardsContainer, { zIndex: 3 } ]}>
-        <View style={styles.card}>
-          <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/31296766?v=4'}}/>
+      <View style={styles.cardsContainer}>
+      { users.length === 0
+      ? <Text style={styles.empty}>Acabou</Text>
+      : (
+        users.map((user, index) =>(
+        <View key={user. _id}style={[ styles.card , { zIndex: users.length - index }]}>
+          <Image style={styles.avatar} source={{ uri: user.avatar }}/>
           <View style={styles.footer}>
-            <Text style={styles.name}>Dino Tudor</Text>
-            <Text style={styles.bio} numberOfLines={3}>Bio </Text>
+            <Text style={styles.name}>{user.name}</Text>
+            <Text style={styles.bio} numberOfLines={3}>{user.bio}</Text>
           </View>
         </View>
+      ))
+    )}
+    </View>
 
-      </View>
-
-      <View style={styles.buttonsContainers}>
-        <TouchableOpacity>
-          <Image style={styles.button} source={dislike}/>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image style={styles.button} source={like}/>
-        </TouchableOpacity>
+    <View style={styles.buttonsContainers}>
+      <TouchableOpacity>
+        <Image style={styles.button} source={dislike}/>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Image style={styles.button} source={like}/>
+      </TouchableOpacity>
       </View>
     </SafeAreaView>
     );
@@ -44,6 +81,9 @@ const styles = StyleSheet.create({
   },
   logo: {
     marginTop: 30,
+  },
+  empty: {
+
   },
 
   cardsContainer: {
